7b5ac6572285d58187f21d4d05b98ac0197a87bb
Artur Mihura <artur.mihura@gmail.com>
2016-02-24 17:42:15 +0300

    update markup formatter

--- markup-formatter.groovy
+++ markup-formatter.groovy
@@ -7,12 +7,10 @@ import org.jenkinsci.plugins.UnsafeMarkupFormatter
   void set_markup(String markup) {
     if (markup == "raw-html") {
       Jenkins.instance.setMarkupFormatter(new RawHtmlMarkupFormatter(false))
-      Jenkins.instance.save()
     } else if (markup == "plain-text") {
       Jenkins.instance.setMarkupFormatter(new EscapedMarkupFormatter())
-      Jenkins.instance.save()
     } else if (markup == "unsafe") {
       Jenkins.instance.setMarkupFormatter(new UnsafeMarkupFormatter())
-      Jenkins.instance.save()
     }
+    Jenkins.instance.save()
   }
